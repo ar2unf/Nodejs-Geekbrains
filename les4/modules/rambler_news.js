e738e0d50e0fa7a7b30ee740bfa4b8ac07181e6e
ar2unf <dim960@gmail.com>
2020-02-17 21:01:40 +0300

    _

--- les4/modules/rambler_news.js
+++ les4/modules/rambler_news.js
@@ -1,5 +1,3 @@
-'use strict';
-
 class Rambler_news {
 
     constructor() {
--- les4/modules/yandex_news.js
+++ les4/modules/yandex_news.js
@@ -1,5 +1,3 @@
-'use strict';
-
 class Yandex_news {
 
     constructor() {
@@ -10,41 +8,26 @@ class Yandex_news {
             politics: 'politics',
         };
     }
-
     /**
      * Функция генерирует ссылку на получение новостей.
-     *
-     * @param category
-     * @returns {string}
-     */
+    */
     generateUrl(category) {
-        console.log(this.baseUrl + this.categories[category] + '.rss');
+       // console.log(this.baseUrl + this.categories[category] + '.rss');
         return this.baseUrl + this.categories[category] + '.rss';
     }
-
     /**
      * Получение списка новостей.
-     *
-     * @param link -- ссылка на список новостей
-     * @param quantity -- количество новостей
-     *
-     * @returns Object -- {title:<>, data: <>} | {error: <>}
      */
     getNews(link, quantity) {
         const request = require('sync-request');
         const parseString = require('xml2js').parseString;
-
         let data = {},
             res = request('GET', link);
-
         if (res.statusCode == 200) {
             let result = res.getBody();
-
             parseString(result, (err, result) => {
                 data.title = result.rss.channel[0].title;
-
                 let news = result.rss.channel[0].item.slice(0, quantity);
-
                 data.data = [];
                 for (let i = 0; i < news.length; ++i) {
                     let item = {
@@ -59,9 +42,7 @@ class Yandex_news {
         else {
             data.error = 'Error occurred while getting news';
         }
-
         return data;
     }
 }
-
 module.exports = Yandex_news;
